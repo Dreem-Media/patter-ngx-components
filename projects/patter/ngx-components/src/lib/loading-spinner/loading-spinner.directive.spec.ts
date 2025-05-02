ea3fa6debb6489eac217b0464f9a4815ec8db7d4
Alex Whitlock <zanderwhitlock@gmail.com>
2025-05-02 21:28:47 +0100

    Fix tests

--- projects/patter/ngx-components/src/lib/loading-spinner/loading-spinner.directive.spec.ts
+++ projects/patter/ngx-components/src/lib/loading-spinner/loading-spinner.directive.spec.ts
@@ -1,8 +1,29 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PtrLoadingSpinnerDirective } from './loading-spinner.directive';
 
+@Component({
+  template: '<div ptrLoadingSpinner="true"></div>',
+  standalone: true,
+  imports: [PtrLoadingSpinnerDirective]
+})
+class TestComponent {}
+
 describe('PtrLoadingSpinnerDirective', () => {
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TestComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    const directive = new PtrLoadingSpinnerDirective();
+    const directiveEl = fixture.debugElement.children[0];
+    const directive = directiveEl.injector.get(PtrLoadingSpinnerDirective);
     expect(directive).toBeTruthy();
   });
 });
--- projects/patter/ngx-components/src/lib/ptr-breadcrumbs/ptr-breadcrumbs.component.spec.ts
+++ projects/patter/ngx-components/src/lib/ptr-breadcrumbs/ptr-breadcrumbs.component.spec.ts
@@ -1,14 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 import { PtrBreadcrumbsComponent } from './ptr-breadcrumbs.component';
 
 describe('PtrBreadcrumbsComponent', () => {
   let component: PtrBreadcrumbsComponent;
   let fixture: ComponentFixture<PtrBreadcrumbsComponent>;
+  let mockRouter: Partial<Router>;
+  let mockActivatedRoute: any;
+  let eventsSubject: BehaviorSubject<NavigationEnd>;
 
   beforeEach(async () => {
+    // Create events subject
+    eventsSubject = new BehaviorSubject<NavigationEnd>(new NavigationEnd(1, '/', '/'));
+
+    // Create mock router with events observable
+    mockRouter = {
+      events: eventsSubject.asObservable()
+    };
+
+    // Create mock activated route with needed properties
+    mockActivatedRoute = {
+      root: {
+        children: []
+      },
+      snapshot: {
+        url: [],
+        data: {},
+        title: ''
+      }
+    };
+
     await TestBed.configureTestingModule({
-      imports: [PtrBreadcrumbsComponent]
+      imports: [PtrBreadcrumbsComponent, RouterModule],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ]
     })
     .compileComponents();
 
